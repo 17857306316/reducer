31460f21055c40e8f2a4fc9a9a30127a25b59a3f
黄亚文 <1102821370@qq.com>
2022-01-05 10:21:58 +0800

    1

--- src/Button.js
+++ src/Button.js
@@ -4,6 +4,8 @@ import {ColorContext} from './color'
 export default function Button() {
     const {dispatch}  = useContext(ColorContext)
 
+
+    //change something
     return (
         <React.Fragment>
             <button onClick={()=>{dispatch({type:'add',color:'red'})}}>add</button>
